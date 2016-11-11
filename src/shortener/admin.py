03cff12dcc2915121eecd4698e59811fa438ad47
Michael Núñez <mike@nunez.pro>
2016-11-10 19:22:37 -0500

    Shortener Admin - Show Custom fields

--- src/shortener/admin.py
+++ src/shortener/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import KirrURL
+#Mostrar los campos que quiero en el admin
+class KirrURLAdmin(admin.ModelAdmin):
+	list_display  = ('url','shortcode','timestamp')		
 
-admin.site.register(KirrURL)
+admin.site.register(KirrURL,KirrURLAdmin)
