f457d0adee108d74937ada28e68d13cf3856d534
Michael Núñez <mike@nunez.pro>
2016-11-11 11:51:11 -0500

    Add function create unique code

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible # only if you need to support Python 2
-from .utils import code_generator
+from .utils import create_shortcode
 
 # Create your models here.
 
 @python_2_unicode_compatible  # only if you need to support Python 2
 class KirrURL(models.Model):
 	url 		= models.CharField(max_length=220, )
-	shortcode 	= models.CharField(max_length= 15, unique=True, null=False, blank=False)
+	shortcode 	= models.CharField(max_length= 15, unique=True, null=False, blank=True)
 	updated		= models.DateTimeField(auto_now=True) #everytime the model is saved
 	timestamp	= models.DateTimeField(auto_now_add=True) #when model was created
 	#empty_datetime 	= models.DateTimeField(auto_now_add=False, auto_now=False) 
@@ -15,8 +15,8 @@ class KirrURL(models.Model):
 	#shortcode = models.CharField(max_length= 15, default='somthing') default value
 
 	def save(self, *args, **kwargs):
-		print('something')
-		self.shortcode = code_generator()
+		if self.shortcode is None or self.shortcode =="":
+			self.shortcode = create_shortcode(self)
 		super(KirrURL,self).save(*args, **kwargs)
 		'''
 		obj = self
--- src/shortener/utils.py
+++ src/shortener/utils.py
@@ -1,6 +1,7 @@
 import random
 import string
 
+#from shotener.models import KirrURL # data error porque importas algo q se importa a si mismo
 
 def code_generator (size=6 ,chars=string.ascii_lowercase + string.digits):
 	return  ''.join(random.choice(chars) for _ in range (size))
@@ -10,3 +11,14 @@ def code_generator (size=6 ,chars=string.ascii_lowercase + string.digits):
 		new_code += randowm.choice(chars)
 	return new_code
 	'''
+
+def create_shortcode (instance):
+	new_code = code_generator()
+	#print(instance) # get de instance name of the created class of model
+	#print(instance.__class__)
+	#print(instance.__class__.__name__)
+	Klass = instance.__class__
+	qs_exists = Klass.objects.filter(shortcode=new_code).exists()
+	if qs_exists:
+		return create_shortcode()
+	return new_code
